3a23912fc9bf49f65d1872abeb20c623d837ca0d
Mohammed Afzaal <mohdafzaal71@gmail.com>
2024-02-17 23:41:18 +0300

    first commit

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
